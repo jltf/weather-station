9b8ecb07724fed0f4d0be33d325b0897605e8d5a
Pavel <pashaivanou@gmail.com>
2023-12-19 00:42:15 +0100

    add graphs to react application

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,65 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import asciichart from 'asciichart';
 
-/* const url = "https://udjs6zb81l.execute-api.eu-north-1.amazonaws.com/serverless_lambda_stage/weather_data";
- */
 const url = process.env.REACT_APP_API_URL;
 
-function App() {
-  const [data, setData] = useState("")
-  const fetchInfo = () => {
-  return fetch(url + "/weather_data")
-    .then((res) => res.json())
-    .then((d) => setData(d.message))
+class DynamicGraph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {temperature_data: [0], humidity_data:[0]};
   }
 
-  useEffect(() => {
-    fetchInfo();
-  }, [])
+  tick() {
+    var fetch_url = url + "/weather_data"
+    var lastTimestamp = this.state.lastTimestamp;
+    if (lastTimestamp) {fetch_url += "?gt=" + lastTimestamp;}
 
+    fetch(fetch_url)
+      .then((res) => res.json())
+      .then((d) => {
+          var temperatures = d.temperatures.map(Number);
+          var humidities = d.humidities.map(Number);
+          var timestamps = d.timestamps;
+
+          this.setState(state => ({
+             temperature_data: state.temperature_data.concat(temperatures),
+             temperature_graph: asciichart.plot(state.temperature_data),
+             humidity_data: state.humidity_data.concat(humidities),
+             humidity_graph: asciichart.plot(state.humidity_data),
+             lastTimestamp: timestamps[timestamps.length - 1] || lastTimestamp,
+          }));
+      });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <>
+      <p>Temperature:</p>
+      <pre><code>
+        {this.state.temperature_graph}
+      </code></pre>
+      <p>Humidity:</p>
+      <pre><code>
+        {this.state.humidity_graph}
+      </code></pre>
+      </>
+    );
+  }
+}
+
+function App() {
   return (
     <div className="App">
-      <p>using JavaScript inbuilt FETCH API</p>
-      <p style={{ fontSize: 20 }}>{data}</p>
+      <DynamicGraph/>
     </div>
   );
 }
